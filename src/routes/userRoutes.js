056cb0e68a4ca9d0d3fc6ebb7294d0606ab0dba6
Eros Linnyker <linnykereros@gmail.com>
2022-06-11 19:25:36 -0300

    Adicionando o middleware do admin nas rotas

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { UsersController } from "../controllers/UserController.js";
 import loginRequired from "../middlewars/loginRequired.js";
+import adminRequired from "../middlewars/adminRequired.js";
 const userRoutes = Router();
 const usersController = new UsersController();
 
@@ -8,8 +9,8 @@ const usersController = new UsersController();
 userRoutes.post("/", usersController.createUser);
 
 //GET
-userRoutes.get("/", loginRequired, usersController.findAllUsers);
-userRoutes.get("/:id", loginRequired, usersController.findUser);
+userRoutes.get("/", loginRequired, adminRequired, usersController.findAllUsers);
+userRoutes.get("/:id", loginRequired, adminRequired, usersController.findUser);
 
 //UPDATE
 userRoutes.put("/:id", loginRequired, usersController.updateUser);
