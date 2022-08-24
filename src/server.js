5e309ba9345c273f88990800efc8c1d754f29afb
Eros Linnyker <linnykereros@gmail.com>
2022-08-24 15:58:47 -0300

    adjusment

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import { router } from "./routes/router.js";
 import cors from "cors";
 const app = express();
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://share-blog-front.vercel.app/",
   credentials: true,
   optionSucessStatus: 200,
 };
