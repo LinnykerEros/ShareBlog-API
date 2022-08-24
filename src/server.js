933af54668ee3cf98a47f804c63067b410afcae3
Eros Linnyker <linnykereros@gmail.com>
2022-08-24 15:35:49 -0300

    added: const port

--- src/server.js
+++ src/server.js
@@ -7,8 +7,9 @@ const corsOptions = {
   credentials: true,
   optionSucessStatus: 200,
 };
+const port = process.env.PORT || 3030;
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
 
-app.listen(3030, () => console.log("Online server at localhost:3030"));
+app.listen(port, () => console.log("Online server at localhost:3030"));
