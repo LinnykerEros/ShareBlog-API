fdfae199b84ebf9169fb4c11586e0b917a9ef9b3
Eros Linnyker <linnykereros@gmail.com>
2022-08-24 16:16:02 -0300

    adjusment: retired obj cors

--- src/server.js
+++ src/server.js
@@ -2,13 +2,9 @@ import express from "express";
 import { router } from "./routes/router.js";
 import cors from "cors";
 const app = express();
-const corsOptions = {
-  origin: "https://share-blog-front-nel1rqoxo-share-blog.vercel.app",
-  credentials: true,
-  optionSucessStatus: 200,
-};
+
 const port = process.env.PORT || 3030;
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
