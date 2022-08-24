3707ab68d409a4a6bdb66fef08bc4c231329c26a
Eros Linnyker <linnykereros@gmail.com>
2022-08-24 14:53:24 -0300

    adjusment: cors

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ import express from "express";
 import { router } from "./routes/router.js";
 import cors from "cors";
 const app = express();
-
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+  optionSucessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
 
